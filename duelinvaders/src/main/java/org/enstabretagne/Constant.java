ff37d3c4e9cd76d8673c7584608a49a0c8b9b1e0
LBF38 <contact.lbf38@gmail.com>
2022-11-28 11:31:27 +0100

    update constants

--- duelinvaders/src/main/java/org/enstabretagne/Constant.java
+++ duelinvaders/src/main/java/org/enstabretagne/Constant.java
@@ -1,10 +1,10 @@
 package org.enstabretagne;
 
 public class Constant {
-    public static final int X=10;
-    public static final int Y=10;
-    public static final int VITESSE_VAISSEAU=1;
-    public static final int VITESSE_TIR=1;
-    public static final int VITESSE_ALIEN=1;
-    public static final int PROBA_ALIEN_TIR=1;
+    public static final int BOARD_WIDTH=10;
+    public static final int BOARD_HEIGHT=10;
+    public static final int SPEED_SPACESHIP=1;
+    public static final int SPEED_SHOOT=1;
+    public static final int SPEED_ALIEN=1;
+    public static final int RATE_ALIEN_SHOOT=1;
 }
