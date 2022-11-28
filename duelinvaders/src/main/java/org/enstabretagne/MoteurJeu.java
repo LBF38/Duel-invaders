ea24005a81e5df9d6eda8d6caac519c905da2b39
LBF38 <contact.lbf38@gmail.com>
2022-11-28 11:11:49 +0100

    Added JavaFX and cleared organisation files
    Co-authored-by: Julia <jufch@users.noreply.github.com>

---
+++ duelinvaders/src/main/java/org/enstabretagne/MoteurJeu.java
@@ -0,0 +1,16 @@
+package org.enstabretagne;
+
+public class MoteurJeu {
+    private boolean play = false;
+
+    public void start() {
+        this.play = true;
+        while (play) {
+            // TODO
+            // todo prendre les éléments rentrant
+            // Todo effectuer les actions
+            // Todo afficher le plateau
+            // todo test fin de jeu
+        }
+    }
+}
