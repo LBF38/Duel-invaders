5e9c2738efa1cbe1b29eeaa27e8007c24017363f
LBF38 <contact.lbf38@gmail.com>
2022-12-23 14:13:01 +0100

    Added MoteurJeu constructor
    First simple constructor of MoteurJeu
    Also added Alien and Player class.
    Not working yet. (WIP)

--- duelinvaders/src/main/java/org/enstabretagne/Alien.java
+++ duelinvaders/src/main/java/org/enstabretagne/Alien.java
@@ -1,4 +1,13 @@
 package org.enstabretagne;
 
-public class Alien {
+public class Alien extends Element {
+    // Defines an alien with a life, speed, etc.
+
+    public Alien() {
+        super(0, 0, 0);
+    }
+
+    public void deplacer() {
+        // TODO
+    }
 }
---
+++ duelinvaders/src/main/java/org/enstabretagne/Player.java
@@ -0,0 +1,19 @@
+package org.enstabretagne;
+
+public class Player extends Element {
+    // Defines a player with a canon, life, score, etc.
+
+    /**
+     * Constructor
+     */
+    public Player() {
+        super(0, 0, 0);
+    }
+
+    /**
+     * Abstract method to redefine.
+     */
+    public void deplacer() {
+        // TODO
+    }
+}
