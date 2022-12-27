b079f2137bb9c10905918895b28ae1c274320ea3
Mathieu_D <mathieu.duflos@hotmail.fr>
2022-12-27 20:46:38 +0100

    Optimization of the shot's movement
    Modification de la classe shot pour mieux utilise les propriétés d'un component sur le modèle de AlienComponent.
    Modifcation des tests en lien.

--- duelinvaders/src/main/java/org/enstabretagne/Component/ShotComponent.java
+++ duelinvaders/src/main/java/org/enstabretagne/Component/ShotComponent.java
@@ -6,15 +6,16 @@ import org.enstabretagne.Core.Constant.Direction;
 import com.almasb.fxgl.entity.component.Component;
 
 public class ShotComponent extends Component {
-    private Double x, y; //position
+    private Double dy; //déplacement
     private Direction direction; // Up or down
 
     public ShotComponent(Double x, Double y, Direction direction) {
         super(); //todo faire test sur x,y et direction
-        this.x = x;
-        this.y = y;
         this.direction = direction;
     }
+
+
+
 
     /*
     Mouvement du tir
@@ -23,8 +24,6 @@ public class ShotComponent extends Component {
     public void onUpdate(double tpf) {
         Double dy = tpf * Constant.SPEED_SHOOT;
         this.move(dy);
-        this.entity.setX(this.x); //Je ne sais pas pourquoi on doit faire ça mais sinon le tir ne bouge pas
-        this.entity.setY(this.y);
     }
 
     public void move(Double dy) throws IllegalArgumentException {
@@ -35,34 +34,11 @@ public class ShotComponent extends Component {
     }
 
     public void moveUp(Double dy) {
-        this.y = this.y - dy;
+        getEntity().translateY(-dy);
     }
 
     public void moveDown(Double dy) {
-        this.y = this.y + dy;
+        getEntity().translateY(dy);
     }
 
-
-    //Getters et Setters todo->conserver ou pas? Car est utile uniquement dans les tests mais est en publiques
-    public Double getX() {
-        return this.x;
-    }
-
-    public Double getY() {
-        return this.y;
-    }
-
-    public void setX(Double x) {
-        if (x < 0 || x > Constant.BOARD_WIDTH) {
-            throw new IllegalArgumentException("x is out of the board");
-        }
-        this.x = x;
-    }
-
-    public void setY(Double y) {
-        if (y < 0 || y > Constant.BOARD_HEIGHT) {
-            throw new IllegalArgumentException("y is out of the board");
-        }
-        this.y = y;
-    }
 }
--- duelinvaders/src/test/java/org/enstabretagne/Component/ShotComponentTest.java
+++ duelinvaders/src/test/java/org/enstabretagne/Component/ShotComponentTest.java
@@ -1,8 +1,12 @@
 package org.enstabretagne.Component;
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import org.enstabretagne.Core.Constant;
 import org.enstabretagne.Core.Constant.Direction;
 import org.junit.jupiter.api.*;
+
+import static com.almasb.fxgl.dsl.FXGL.entityBuilder;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ShotComponentTest {
@@ -11,53 +15,35 @@ public class ShotComponentTest {
 
     @BeforeEach
     public void setup() {
-        this.shotComponent = new ShotComponent(0.0, 0.0, Constant.Direction.UP);
+
+        shotComponent = new ShotComponent(0.0, 0.0, Constant.Direction.UP);
+        entityBuilder()
+                .type(EntityType.SHOT)
+                .at(0, 0)
+                .with(shotComponent)
+                .build();
     }
 
     @Test
     public void initializationTest() {
-        assertEquals(0.0, this.shotComponent.getX());
-        assertEquals(0.0, this.shotComponent.getY());
+        assertEquals(0.0, this.shotComponent.getEntity().getX());
+        assertEquals(0.0, this.shotComponent.getEntity().getY());
     }
 
     @Test
-    @DisplayName("Test de la propriété x")
-    public void testX() {
-        assertNotEquals(null, shotComponent.getX());
-        shotComponent.setX(5.0);
-        assertEquals(5, shotComponent.getX());
-    }
-
-    @Test
-    @DisplayName("Test de la propriété y")
-    public void testY() {
-        assertNotEquals(null, shotComponent.getY());
-        shotComponent.setY(5.0);
-        assertEquals(5, shotComponent.getY());
-    }
-
-    @Test
-    @DisplayName("Tests des limites de jeu")
-    public void testExceptions() {
-        assertThrows(IllegalArgumentException.class, () -> shotComponent.setX(-1.0));
-        assertThrows(IllegalArgumentException.class, () -> shotComponent.setX(Constant.BOARD_WIDTH + 1.0));
-        assertThrows(IllegalArgumentException.class, () -> shotComponent.setY(-1.0));
-        assertThrows(IllegalArgumentException.class, () -> shotComponent.setY(Constant.BOARD_HEIGHT + 1.0));
-    }
-
-    @Test
-    @DisplayName("Déplacement du shotComponent")
+    @DisplayName("Déplacement du shotComponent vers le haut")
     public void moveUp() {
-        shotComponent.setY(dy);
+        shotComponent.getEntity().setY(dy);
         shotComponent.moveUp(dy);
-        assertEquals(0, shotComponent.getY());
+        assertEquals(0, shotComponent.getEntity().getY());
     }
 
     @Test
+    @DisplayName("Déplacement du shotComponent vers le bas")
     public void moveDown() {
-        assertEquals(0, shotComponent.getY());
+        assertEquals(0, shotComponent.getEntity().getY());
         shotComponent.moveDown(dy);
-        assertEquals(dy, shotComponent.getY());
+        assertEquals(dy, shotComponent.getEntity().getY());
     }
 
 }
