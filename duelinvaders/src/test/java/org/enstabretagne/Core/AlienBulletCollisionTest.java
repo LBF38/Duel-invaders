92a501893451fff11665dfcabfe5eeb37e227fac
Julia Fouchier <julia.fouchier@hotmail.fr>
2023-01-01 19:51:19 +0100

    Test files Not working yet

--- duelinvaders/src/test/java/org/enstabretagne/Core/AlienBulletCollisionTest.java
+++ duelinvaders/src/test/java/org/enstabretagne/Core/AlienBulletCollisionTest.java
@@ -18,7 +18,15 @@ import static com.almasb.fxgl.dsl.FXGL.*;
 
 
 public class AlienBulletCollisionTest {
+/*
+    @BeforeEach
+    public void setup(Map<String, Object> vars) {
+        vars.put(GameVariableNames.PLAYER1_SCORE, 0);
+        System.out.println("GameVariableNames.PLAYER1_SCORE = " + vars.get(GameVariableNames.PLAYER1_SCORE));
+    }
+*/
 
+    /*
     @Test
     void initialization() {
         var collision_alien_bullet = new AlienBulletCollision(EntityType.ALIEN, EntityType.BULLET);
@@ -32,18 +40,26 @@ public class AlienBulletCollisionTest {
 
     @Test
     void BonusCollision(){
+        var collision_alien_bullet = new AlienBulletCollision(EntityType.ALIEN, EntityType.BULLET);
+        Entity bullet = entityBuilder().type(EntityType.BULLET).build();
+        Entity alien = entityBuilder().type(EntityType.ALIEN).build();
+        collision_alien_bullet.onCollisionBegin(bullet,alien);
+        assertEquals(1, geti(GameVariableNames.PLAYER1_SCORE));
+    }
 
+
+        //System.out.println(getWorldProperties().intProperty(GameVariableNames.PLAYER1_SCORE).asString());
         //vars.put(GameVariableNames.PLAYER1_SCORE, 0);
         //var GameVariableNames = new GameVariableNames();
-        GameVariableNames.PLAYER1_SCORE = "0";
+        //GameVariableNames.PLAYER1_SCORE = "0";
         //vars.put(GameVariableNames.PLAYER1_SCORE, 0);
-        assertEquals(GameVariableNames.PLAYER1_SCORE, String.valueOf(0));
+        //assertEquals(GameVariableNames.PLAYER1_SCORE, String.valueOf(0));
 
         //assert GameVariableNames.PLAYER1_SCORE = String.valueOf(0);
-        var collision_alien_bullet = new AlienBulletCollision(EntityType.ALIEN, EntityType.BULLET);
-        System.out.println(GameVariableNames.PLAYER1_SCORE);
+        //var collision_alien_bullet = new AlienBulletCollision(EntityType.ALIEN, EntityType.BULLET);
+        //System.out.println(GameVariableNames.PLAYER1_SCORE);
         //assertEquals(GameVariableNames.PLAYER1_SCORE, String.valueOf(1));
-    }
+        //assertEquals(GameVariableNames.PLAYER1_SCORE, String.valueOf(0));
 
     @Test
     void AlienRemoveTest(){
@@ -51,6 +67,38 @@ public class AlienBulletCollisionTest {
 
         var collision_alien_bullet = new AlienBulletCollision(EntityType.ALIEN, EntityType.BULLET);
         //assertEquals(getGameWorld().getEntitiesByType(EntityType.ALIEN).isEmpty(), true);
+        assert getGameWorld().getEntitiesByType(EntityType.ALIEN).isEmpty();
+    }
+
+    @Test
+    void BulletRemoveTest(){
+        var collision_alien_bullet = new AlienBulletCollision(EntityType.ALIEN, EntityType.BULLET);
+        assert getGameWorld().getEntitiesByType(EntityType.BULLET).isEmpty();
+    }
+
+}
+
+     */
+
+
+    @BeforeEach
+    public void setup(Map<String, Object> vars) {
+        vars.put(GameVariableNames.PLAYER1_SCORE, 0);
+        System.out.println("GameVariableNames.PLAYER1_SCORE = " + vars.get(GameVariableNames.PLAYER1_SCORE));
+    }
+
+    @Test
+    void BonusCollision(){
+        var collision_alien_bullet = new AlienBulletCollision(EntityType.ALIEN, EntityType.BULLET);
+        Entity bullet = entityBuilder().type(EntityType.BULLET).build();
+        Entity alien = entityBuilder().type(EntityType.ALIEN).build();
+        collision_alien_bullet.onCollisionBegin(bullet,alien);
+        assertEquals(1, geti(GameVariableNames.PLAYER1_SCORE));
+    }
+
+    @Test
+    void AlienRemoveTest(){
+        var collision_alien_bullet = new AlienBulletCollision(EntityType.ALIEN, EntityType.BULLET);
         assert getGameWorld().getEntitiesByType(EntityType.ALIEN).isEmpty();
     }
 
--- duelinvaders/src/test/java/org/enstabretagne/Core/AlienPlayerCollisionTest.java
+++ duelinvaders/src/test/java/org/enstabretagne/Core/AlienPlayerCollisionTest.java
@@ -1,24 +1,59 @@
 package org.enstabretagne.Core;
 
+import org.enstabretagne.Component.AlienComponent;
+import org.enstabretagne.Component.PlayerComponent;
 import org.enstabretagne.Component.EntityType;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static com.almasb.fxgl.dsl.FXGL.entityBuilder;
+import static com.almasb.fxgl.dsl.FXGLForKtKt.getWorldProperties;
 
 public class AlienPlayerCollisionTest {
     private AlienPlayerCollision collision_alien_player;
 
-
+/*
     @BeforeEach
     public void setUp() {
         collision_alien_player = new AlienPlayerCollision(EntityType.PLAYER, EntityType.ALIEN);
     }
+*/
 
+    private AlienComponent alienComponent;
+    private PlayerComponent playerComponent;
+
+    @BeforeEach
+    public void setup() {
+        alienComponent = new AlienComponent(Constant.Direction.DOWN);
+        entityBuilder()
+            .type(EntityType.ALIEN)
+            .at(0, 0)
+            .with(alienComponent)
+            .build();
+
+        playerComponent = new PlayerComponent();
+        entityBuilder()
+            .type(EntityType.PLAYER)
+            .at(0, 0)
+            .with(playerComponent)
+            .build();
+    }
     @Test
     void initialization() {
         assert collision_alien_player != null;
-
-
     }
 
+    @Test
+    void testRemoveFromWorld(){
+        //getWorldProperties();
+        assert playerComponent != null;
+        assert alienComponent != null;
+
+        collision_alien_player.onCollisionBegin(playerComponent.getEntity(), alienComponent.getEntity());
+        //var collision = collision_alien_player.onCollisionBegin(playerComponent.getEntity(), alienComponent.getEntity());
+        assert playerComponent.getEntity() == null;
+        assert alienComponent.getEntity() != null;
+    }
 
 }
