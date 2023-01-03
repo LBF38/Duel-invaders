048d0b7a34c329ce1179942836776ceacf184431
LBF38 <contact.lbf38@gmail.com>
2023-01-03 01:14:50 +0100

    Cleaned LifeComponent

--- duelinvaders/src/main/java/org/enstabretagne/Component/LifeComponent.java
+++ duelinvaders/src/main/java/org/enstabretagne/Component/LifeComponent.java
@@ -2,43 +2,29 @@ package org.enstabretagne.Component;
 
 import org.enstabretagne.Core.Constant;
 
-import com.almasb.fxgl.dsl.components.ExpireCleanComponent;
 import com.almasb.fxgl.entity.component.Component;
-
-import javafx.util.Duration;
-import org.enstabretagne.Core.GameVariableNames;
 
 /**
  * Cette classe représente l'affichage du nombre de vies restantes.
  *
- * @author @MathieuDFS, @jufch
+ * @author MathieuDFS, jufch
+ * @since 0.2.0
  */
-public class LifeComponent extends Component{
-    private Constant.Direction direction;
-    private boolean isActive = true;
+public class LifeComponent extends Component {
     /**
      * Initialise le composant affichage de vie
      */
     public void initialize(Constant.Direction direction) {
-        this.direction = direction;
-        if(direction == Constant.Direction.UP){
-            entity.setX(Constant.GAME_WIDTH-Constant.LIFE_DISPLAY_WIDTH);
+        if (direction == Constant.Direction.UP) {
+            entity.setX(Constant.GAME_WIDTH - Constant.LIFE_DISPLAY_WIDTH);
             entity.setY(0);
-        }
-        else if(direction == Constant.Direction.DOWN){
-            entity.setX(Constant.GAME_WIDTH-Constant.LIFE_DISPLAY_WIDTH);
-            entity.setY(Constant.GAME_HEIGHT-Constant.LIFE_DISPLAY_HEIGHT);
+        } else if (direction == Constant.Direction.DOWN) {
+            entity.setX(Constant.GAME_WIDTH - Constant.LIFE_DISPLAY_WIDTH);
+            entity.setY(Constant.GAME_HEIGHT - Constant.LIFE_DISPLAY_HEIGHT);
         }
     }
 
-    public void updateLife(boolean isActive){
-        this.isActive = isActive;
-        if(this.isActive){
-            entity.getViewComponent().setOpacity(1);
-        }
-        else{
-            entity.getViewComponent().setOpacity(0);
-        }
+    public void updateLife(boolean isActive) {
+        entity.getViewComponent().setOpacity(isActive ? 1 : 0);
     }
 }
-
