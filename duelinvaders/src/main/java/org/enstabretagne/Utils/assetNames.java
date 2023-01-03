23e72ddd235cc24aaf29d0ccefc123ac84697f79
LBF38 <contact.lbf38@gmail.com>
2023-01-03 03:15:58 +0100

    Cleaned assetNames

--- duelinvaders/src/main/java/org/enstabretagne/Utils/assetNames.java
+++ duelinvaders/src/main/java/org/enstabretagne/Utils/assetNames.java
@@ -6,7 +6,8 @@ import org.enstabretagne.Core.Constant;
 
 /**
  * Classe contenant les noms des assets du jeu
- * Cela permet d'éviter les risques de fautes de frappe et centralise l'ensemble des noms d'assets du jeu
+ * Cela permet d'éviter les risques de fautes de frappe et centralise l'ensemble
+ * des noms d'assets du jeu
  * 
  * @author LBF38, MathieuDFS, jufch
  * @since 0.2.0
@@ -22,18 +23,20 @@ public class assetNames {
         public static final String BACKGROUND = "background.png";
         public static final String EXPLOSION_PLAYER = "explosion_player.png";
         public static final String EXPLOSION_FINAL = "explosion_final.png";
-        public static final ArrayList<String> EXPLOSIONS = new ArrayList<String>();
-        static {
-            for (int i = 1; i <= Constant.NUMBER_OF_EXPLOSIONS; i++) {
-                EXPLOSIONS.add("explosion" + i + ".png");
+        public static final ArrayList<String> EXPLOSIONS = new ArrayList<String>() {
+            {
+                for (int i = 1; i <= Constant.NUMBER_OF_EXPLOSIONS; i++) {
+                    add("explosion" + i + ".png");
+                }
             }
-        }
-        public static final ArrayList<String> LIFES = new ArrayList<String>();
-        static {
-            for (int i = 1; i <= 3; i++) {
-                LIFES.add("life" + i + ".png");
+        };
+        public static final ArrayList<String> LIFES = new ArrayList<String>() {
+            {
+                for (int i = 1; i <= 3; i++) {
+                    add("life" + i + ".png");
+                }
             }
-        }
+        };
         public static final String FIRE = "fire.png";
         public static final String SMOKE = "smoke.png";
     }
@@ -54,18 +57,20 @@ public class assetNames {
         public static final String START_CLAIRON = "autre/claironStart.wav";
         public static final String DEFEAT_CLAIRON = "autre/claironDefeat.wav";
         public static final String VICTORY_CLAIRON = "autre/claironVictory.wav";
-        public static final ArrayList<String> AMBIENT_SOUNDS = new ArrayList<String>();
-        static {
-            for (int i = 1; i < Constant.NUMBER_OF_AMBIENT_SOUND + 1; i++) {
-                AMBIENT_SOUNDS.add("ambiance/ambientSound" + i + ".wav");
+        public static final ArrayList<String> AMBIENT_SOUNDS = new ArrayList<String>() {
+            {
+                for (int i = 1; i < Constant.NUMBER_OF_AMBIENT_SOUND + 1; i++) {
+                    add("ambiance/ambientSound" + i + ".wav");
+                }
             }
-        }
+        };
         public static final String CANNON_SHOT = "Tir/canon.wav";
-        public static final ArrayList<String> LASER_SOUNDS = new ArrayList<>();
-        static {
-            for (int i = 1; i <= Constant.NUMBER_OF_LASER_SOUNDS; i++) {
-                LASER_SOUNDS.add("Tir/laser" + i + ".wav");
+        public static final ArrayList<String> LASER_SOUNDS = new ArrayList<>() {
+            {
+                for (int i = 1; i <= Constant.NUMBER_OF_LASER_SOUNDS; i++) {
+                    add("Tir/laser" + i + ".wav");
+                }
             }
-        }
+        };
     }
 }
