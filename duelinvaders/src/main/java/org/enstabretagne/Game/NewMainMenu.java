1066b8911ac57d2c78f02e2b254972ba829d3b0e
LBF38 <contact.lbf38@gmail.com>
2023-01-03 04:20:50 +0100

    Cleaned NewMainMenu

--- duelinvaders/src/main/java/org/enstabretagne/Game/NewMainMenu.java
+++ duelinvaders/src/main/java/org/enstabretagne/Game/NewMainMenu.java
@@ -1,9 +1,13 @@
 package org.enstabretagne.Game;
 
+import static com.almasb.fxgl.dsl.FXGL.getUIFactoryService;
+
+import org.enstabretagne.Core.Constant;
+
 import com.almasb.fxgl.app.scene.FXGLMenu;
 import com.almasb.fxgl.app.scene.MenuType;
 import com.almasb.fxgl.dsl.FXGL;
-import com.almasb.fxgl.texture.Texture;
+
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.StringBinding;
 import javafx.geometry.Orientation;
@@ -12,16 +16,11 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Separator;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
-import org.enstabretagne.Core.Constant;
-
-import static com.almasb.fxgl.dsl.FXGLForKtKt.getUIFactoryService;
 
 
 public class NewMainMenu extends FXGLMenu {
@@ -29,13 +28,13 @@ public class NewMainMenu extends FXGLMenu {
         super(MenuType.MAIN_MENU);
 
         SpaceButton buttonPlayGameClassique = new SpaceButton("Play Classique",()->{
-            GameLauncher.setGameMode(0);
+            GameLauncher.setGameMode(GameMode.CLASSIQUE);
             fireNewGame();});
         SpaceButton buttonPlayGameInfini = new SpaceButton("Play Infinity",()->{
-            GameLauncher.setGameMode(1);
+            GameLauncher.setGameMode(GameMode.INFINITY_MODE);
             fireNewGame();});
         SpaceButton buttonPlayGameSolo = new SpaceButton("Play Solo",()-> {
-            GameLauncher.setGameMode(2);
+            GameLauncher.setGameMode(GameMode.SOLO);
             fireNewGame();});
         /*SpaceButton buttonOption = new SpaceButton("Option",()-> {
         });
@@ -90,15 +89,10 @@ public class NewMainMenu extends FXGLMenu {
     private static class SpaceButton extends StackPane {
         private static final Color SELECTED_COLOR = Color.BLACK; //Color.WHITE;
         private static final Color NOT_SELECTED8COLOR = Color.GRAY;
-        private String name;
-        private Runnable action;
         private Text text;
         private Rectangle selector;
 
         public SpaceButton(String name, Runnable action) {
-            this.name = name;
-            this.action = action;
-
             text=FXGL.getUIFactoryService().newText(name, Color.BLACK,16.0);
             text.fillProperty().bind(
                     Bindings.when(focusedProperty())
